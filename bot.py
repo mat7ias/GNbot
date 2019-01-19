068cf97eaca1d1c216501d34a91dc00ff6edf65b
Mattias Nystrom <35585644+mat7ias@users.noreply.github.com>
2019-01-19 19:40:10 +1100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,149 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+from time import time
+import time
+import os
+from pprint import pprint
+import sys
+import yaml
+import telegram
+
+#### Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - '
+                    '%(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Configure Logging
+
+FORMAT = '%(asctime)s -- %(levelname)s -- %(module)s %(lineno)d -- %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+logger = logging.getLogger('root')
+logger.info("Running "+sys.argv[0])
+
+##### Open config_file
+config = None
+if os.path.isfile("config.ymal"):
+    with open("config.ymal") as config_file:
+        config = yaml.load(config_file)
+else:
+    exit("No configuration file 'config.yaml' found")
+    sys.exit()
+
+##### load config
+bot_token = config['bot_token']
+bot = telegram.Bot(token=bot_token)
+
+def get_name(user):
+        try:
+            name = user.first_name
+        except (NameError, AttributeError):
+            try:
+                name = user.username
+            except (NameError, AttributeError):
+                logger.info("No username or first name")
+                return	""
+        return name
+
+################################ Commands ######################################
+
+def start(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    message_id = update.message.message_id
+    chat_id = update.message.chat.id
+    if (update.message.chat.type == 'group') or (update.message.chat.type == 'supergroup'):
+        msg = config['pmme']
+        bot.sendMessage(chat_id=chat_id,text=msg,reply_to_message_id=message_id, parse_mode="Markdown",disable_web_page_preview=1)
+    else:
+        msg = config['start']
+        update.message.reply_text("Hey "+str(update.message.chat.first_name)+"! Get a list of my commands with /commands")
+
+def commands(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['commands']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def extras(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['extras']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def resources(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['resources']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def events(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['events']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def videos(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['videos']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def rules(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['rules']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def adminlist(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['adminlist']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def releases(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+    msg = config['releases']
+    bot.sendMessage(chat_id=chat_id,text=msg,parse_mode="Markdown",disable_web_page_preview=1)
+
+def carlos(bot, update):
+    pprint(update.message.chat.__dict__, indent=4)
+    chat_id = update.message.chat.id
+	msg = bot.sendPhoto(chat_id=chat_id, photo=open("carlos.png",'rb'), caption="Bitconnect" )
+
+###############################################################################
+
+###### Error logging
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+###### Running the bot
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    print("Bot started")
+    updater = Updater(bot_token)
+
+##### Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+##### CommandHandlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("commands", commands))
+    dp.add_handler(CommandHandler("extras", extras))
+    dp.add_handler(CommandHandler("resources", resources))
+    dp.add_handler(CommandHandler("events", events))
+    dp.add_handler(CommandHandler("videos", videos))
+    dp.add_handler(CommandHandler("rules", rules))
+    dp.add_handler(CommandHandler("adminlist", adminlist))
+    dp.add_handler(CommandHandler("releases", releases))
+    dp.add_handler(CommandHandler("carlos", carlos))
+
+##### Log all errors
+    dp.add_error_handler(error)
+
+# Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
